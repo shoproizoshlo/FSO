9e8b6cc6c8874ce5d8b318b1e54819ad9abdcc63
shoproizoshlo <iamsusunny@gmail.com>
2023-12-04 16:30:53 +0100

    displaying the statistics is extracted into its own Statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Statistics from "./Statistics";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,9 +17,7 @@ const App = () => {
     setBad(bad + 1);
   };
 
-  const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = (good / all) * 100;
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,13 +25,7 @@ const App = () => {
       <button onClick={setToZero}>neutral</button>
       <button onClick={decreaseByOne}>bad</button>
 
-      <h2>give feedback</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,25 @@
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      {all <= 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive}%</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Statistics;
