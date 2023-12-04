beb60347631743529f8b24ef920349e530c8c3fe
shoproizoshlo <iamsusunny@gmail.com>
2023-12-04 16:15:42 +0100

    Expanded application so that it shows more statistics about the gathered feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,17 +6,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const increaseByOne = () => {
+    setGood(good + 1);
+  };
+  const setToZero = () => {
+    setNeutral(neutral + 1);
+  };
+  const decreaseByOne = () => {
+    setBad(bad + 1);
+  };
+
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={increaseByOne}>good</button>
+      <button onClick={setToZero}>neutral</button>
+      <button onClick={decreaseByOne}>bad</button>
 
       <h2>give feedback</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
