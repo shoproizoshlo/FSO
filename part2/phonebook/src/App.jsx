f7de2d07caa97044e9103a7d7f82c6db893dcffe
shoproizoshlo <iamsusunny@gmail.com>
2023-12-07 12:03:46 +0100

    made validation insensitive

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -20,7 +25,11 @@ const App = () => {
       id: persons.length + 1,
     };
 
-    if (persons.some((person) => person.name === newName)) {
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat(nameObject));
@@ -34,6 +43,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <h2>Add new number</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
