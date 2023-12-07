2b9fa8fec625cd8052c3c296b8641136ac0fb266
shoproizoshlo <iamsusunny@gmail.com>
2023-12-07 10:52:12 +0100

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,27 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+    console.log(newName);
+  };
+
+  const addNewName = (e) => {
+    e.preventDefault();
+    setPersons(newName);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
     </div>
   );
 };
