4f18f58ea61aad8f26d89ce2108d8fddc7d02ced
shoproizoshlo <iamsusunny@gmail.com>
2023-12-21 12:13:59 +0100

    Update persons.js

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,15 +2,17 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl);
+  return axios.get(baseUrl).then((response) => response.data);
 };
 
 const create = (newObject) => {
-  return axios.post(baseUrl, newObject);
+  return axios.post(baseUrl, newObject).then((response) => response.data);
 };
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject);
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then((response) => response.data);
 };
 
 export default { getAll, create, update };
