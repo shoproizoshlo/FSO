0b0fe88c3ae651be87531966f1162c7b658fed39
shoproizoshlo <iamsusunny@gmail.com>
2023-12-25 21:37:31 +0100

    added axios

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+import countryService from "./services/countryService";
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  const [countries, setCountries] = useState("");
+
+  useEffect(() => {
+    countryService.getAll().then((response) => {
+      setCountries(response.data);
+      console.log(response.data);
+    });
+  });
+  return (
+    <>
+      <label htmlFor="search">find countries</label>
+      <input type="text" id="countries" />
+      <ul></ul>
+      <p>Too many matches, specify search</p>
+    </>
+  );
 }
 
 export default App;
