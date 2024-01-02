91417825b81bf9ddb4a9e842eb07bfe73ee0909d
shoproizoshlo <iamsusunny@gmail.com>
2024-01-02 10:09:44 +0100

    Update App.jsx

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -8,15 +8,14 @@ function App() {
   useEffect(() => {
     countryService.getAll().then((response) => {
       setCountries(response.data);
-      console.log(response.data);
     });
-  });
+  }, []);
+
   return (
     <>
       <label htmlFor="search">find countries</label>
       <input type="text" id="countries" />
       <ul></ul>
-      <p>Too many matches, specify search</p>
     </>
   );
 }
